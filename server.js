a7257d6df5ce2e12a3100142a80ee186007a7f26
Daes-inc <book4funds@gmail.com>
2026-03-16 00:14:47 -0700

    Add JSON parsing and new POST endpoint

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Dakota AI Builder is running");
 });
 
+app.post("/run", (req, res) => {
+
+  const command = req.body.command;
+
+  console.log("Command received:", command);
+
+  res.json({
+    status: "command received",
+    command: command
+  });
+
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log("Server running on port", PORT);
 });
